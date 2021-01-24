06ed98157d46842b2761bb9aad3fd516a522a437
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-24 18:58:56 +0300

    add home view function

--- user/views.py
+++ user/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+def home(request):
+    projects = project.objects.all()
+
+    return render(request, 'home.html' , {'projects':projects})
