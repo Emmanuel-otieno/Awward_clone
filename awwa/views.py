d1ac407708c367265b8565a0b8e9c28122de5992
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-24 19:09:02 +0300

    add new  view function of posting projects

--- awwa/views.py
+++ awwa/views.py
@@ -7,4 +7,20 @@ def home(request):
     return render(request, 'home.html' , {'projects':projects})
 
 def about(request):
-     return render(request, 'about.html',)    
+     return render(request, 'about.html',)
+
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.editor = current_user
+            project.save()
+        return redirect('home')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'add-project.html', {"form": form})     
