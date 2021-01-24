e1df91b334a060c9743b8c04fe23e944a3957fb9
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-24 20:20:05 +0300

    update models file

--- awwa/models.py
+++ awwa/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='new_project/')
+    description = models.TextField(max_length=200)
+    link = models.URLField(max_length=200)
+    date = models.DateField(auto_now_add=True)
+    editor =models.ForeignKey(User,on_delete=models.CASCADE, default='')
+
+        
+    def __str__(self):
+        return self.
