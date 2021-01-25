8741b6f7a587c0fea8add3f79e12b9a1f8c92ca5
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-25 15:46:46 +0300

    add test to the models

--- awwa/tests.py
+++ awwa/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
-
+from .models import Project
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class ProjectTestClass(TestCase):
+    '''
+    Test case for the Project class and it's behaviours
+    '''
+
+    def setUp(self):
+        return Projects.objects.create()
+
+        #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Chi_Gallery,Project))
+
+        #Testing save method
+        def test_save_method(self):
+            self.Chi_Gallery.save_project()
+            projects = Project.objects.all()
+            self.assertTrue(len(projects) > 0)
+
+    def setUp(self,project_name='Chi_Gallery',project_photo='tree.png',description='some description',github_repo='git repo',url='tree.com',uploader='chinchillah'):
+        return Projects.objects.create(project_name=project_name, project_photo=project_photo, description=description, github_repo=github_repo, url=url, uploader=uploader)
+
+    def projectSave(self):
+        initialization = self.setUp()
+        self.assertTrue(save > 0)
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    '''
+    Test case for the Profile class and it's behaviours
+    '''
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    # Testing instance
+    def test_instance(self):
+        '''
+        Test to confirm that the object is being instantiated correctly
+        '''
+        self.assertTrue(isinstance(self.new_profile, Profile))
+
+    def test_save_method(self):
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)
+
+    def test_delete_profile(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) == 0)
+
+
+if __name__ == "__main__":
+    main()
