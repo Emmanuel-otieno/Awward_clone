8fbd2cb5c0d0c5dd5c7fbb10eb8471312607bb26
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-25 18:32:36 +0300

    Deployment

--- awward/settings.py
+++ awward/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b_x!la_1=39e^#i#y4h)j&1c66+v_qeshvv@s#z8r%4s+50!q0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
