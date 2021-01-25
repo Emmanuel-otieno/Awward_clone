3bd6beebf8f278068e3ffedf5b64ebb27b8a9017
Emmanuel-otieno <sakoemmanuel4@gmail.com>
2021-01-25 08:54:19 +0300

    update my view function

--- awwa/views.py
+++ awwa/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
-
+from django .http import HttpResponse
+from.models import Project
+from .forms import NewProjectForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     projects = project.objects.all()
--- awward/urls.py
+++ awward/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import path,include
+from django.conf.urls import url,include
 from django.contrib import admin
 from user import views as user_views
 from django.contrib.auth import views as auth_views
@@ -21,13 +21,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('tinymce/', include('tinymce.urls')),
-    path('',include('awwa.urls')),
+    url('admin/', admin.site.urls),
+    url('register/', user_views.register, name='register'),
+    url('profile/', user_views.profile, name='profile'),
+    url('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    url('tinymce/', include('tinymce.urls')),
+    url('',include('awwa.urls')),
 ]
 
 if settings.DEBUG:
